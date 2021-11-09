6a5b18dca5e6d1ee214f4eba645914c860431ee1
katylorny <katy_lorny@mail.ru>
2021-11-09 19:55:35 +0300

    getting data from json, loading data into store

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -4,17 +4,33 @@
 
 <script>
 import mapboxgl from 'mapbox-gl';
+import {mapMutations} from "vuex";
 
 export default {
   name: "Map",
+
   mounted() {
-    mapboxgl.accessToken = 'pk.eyJ1Ijoia2F0eWxvcm55IiwiYSI6ImNrdnNhZDFjcmIxczgyb3M3azl6ZG8xamEifQ.egE0UVDX4gVCMuHly5a5gw';
-    new mapboxgl.Map({
-      container: 'map', // container ID
-      style: 'mapbox://styles/mapbox/streets-v11', // style URL
-      center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9 // starting zoom
-    });
+    fetch(`./metro.json`)
+        .then(response => response.json())
+        .then((response) => {
+          console.log(response);
+          this.setStations(response)
+        })
+    this.initMap()
+  },
+  methods: {
+    ...mapMutations([
+        'setStations'
+    ]),
+    initMap() {
+      mapboxgl.accessToken = 'pk.eyJ1Ijoia2F0eWxvcm55IiwiYSI6ImNrdnNhZDFjcmIxczgyb3M3azl6ZG8xamEifQ.egE0UVDX4gVCMuHly5a5gw';
+      new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/streets-v11', // style URL
+        center: [37.6030121, 55.88387], // starting position [lng, lat]
+        zoom: 15 // starting zoom
+      });
+    },
   }
 }
 
--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,14 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        stations: []
+    },
+    mutations: {
+        setStations(state, stations) {
+            state.stations = stations
+        }
+    },
+    actions: {},
+    modules: {}
 })
