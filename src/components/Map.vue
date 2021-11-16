b0137454386d7b54d04d19be4d8b86bf57499c84
katylorny <katy_lorny@mail.ru>
2021-11-16 15:43:38 +0300

    filter stops on map

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,15 +1,20 @@
 <template>
-    <div id="map"></div>
+  <div id="map"></div>
 </template>
 
 <script>
 import mapboxgl from 'mapbox-gl';
-import {mapMutations} from "vuex";
+import {mapGetters, mapMutations} from "vuex";
 import {eventBus} from "../main";
 
 export default {
   name: "Map",
 
+  data() {
+    return {
+      map: null
+    }
+  },
   mounted() {
     fetch(`./metro.json`)
         .then(response => response.json())
@@ -26,11 +31,50 @@ export default {
           this.setStops(response)
         })
   },
+  computed: {
+    ...mapGetters([
+      'shownStops',
+      'stationsGeojson'
+    ])
+  },
+  watch: {
+    shownStops: function () {
+      if (this.map.isStyleLoaded()) {
+        this.map.removeLayer('stops')
+        this.map.removeSource('stopsData')
+        this.setStopsLayer()
+      }
+    }
+  },
+
   methods: {
     ...mapMutations([
       'setStations',
       'setStops'
     ]),
+
+    setStopsLayer() {
+      this.map.addSource(`stopsData`, {
+        type: `geojson`,
+        data: {
+          type: 'FeatureCollection',
+          features: this.shownStops
+        }
+      })
+
+      this.map.addLayer({
+        'id': 'stops',
+        'type': 'circle',
+        'layout': {
+          'visibility': 'visible'
+        },
+        'source': 'stopsData',
+        'paint': {
+          'circle-radius': 8,
+          'circle-color': 'black'
+        }
+      });
+    },
     getActiveStationId(id) {
       eventBus.$emit(`onStationClick`, {
         activeStationId: id,
@@ -39,25 +83,26 @@ export default {
     initMap() {
       mapboxgl.accessToken = 'pk.eyJ1Ijoia2F0eWxvcm55IiwiYSI6ImNrdnNhZDFjcmIxczgyb3M3azl6ZG8xamEifQ.egE0UVDX4gVCMuHly5a5gw';
 
-      const map = new mapboxgl.Map({
+      this.map = new mapboxgl.Map({
         container: 'map', // container ID
         style: 'mapbox://styles/mapbox/streets-v11', // style URL
-        center: [37.6156, 55.7522],
+        // center: [37.6156, 55.7522],
+        center: [128.218463048, 51.496981567],
         zoom: 10 // starting zoom
       })
 
 
-      map.on('load', () => {
+      this.map.on('load', () => {
 
-        map.addSource(`stationsData`, {
+        this.map.addSource(`stationsData`, {
           type: `geojson`,
           data: {
             type: 'FeatureCollection',
-            features: this.$store.getters.stationsGeojson
+            features: this.stationsGeojson
           }
         })
 
-        map.addLayer({
+        this.map.addLayer({
           'id': 'stations',
           'type': 'circle',
           'layout': {
@@ -70,36 +115,16 @@ export default {
           }
         });
 
-
-        map.on(`click`, `stations`, (e) => {
+        this.map.on(`click`, `stations`, (e) => {
           this.getActiveStationId(e.features[0].properties.id)
         })
 
-        map.addSource(`stopsData`, {
-          type: `geojson`,
-          data: {
-            type: 'FeatureCollection',
-            features: this.$store.getters.shownStops
-          }
-        })
-
-        map.addLayer({
-          'id': 'stops',
-          'type': 'circle',
-          'layout': {
-            'visibility': 'visible'
-          },
-          'source': 'stopsData',
-          'paint': {
-            'circle-radius': 8,
-            'circle-color': 'black'
-          }
-        });
+        this.setStopsLayer()
 
 
       });
-      map.on('idle', () => {
-        if (!map.getLayer('stops') || !map.getLayer('stations')) {
+      this.map.on('idle', () => {
+        if (!this.map.getLayer('stops') || !this.map.getLayer('stations')) {
           return;
         }
 
@@ -107,7 +132,6 @@ export default {
 
         for (const id of toggleableLayerIds) {
           if (document.getElementById(id)) {
-            console.log(`document.getElementById(id)`, document.getElementById(id));
             continue;
           }
 
@@ -118,23 +142,23 @@ export default {
           link.className = 'active';
 
 // Show or hide layer when the toggle is clicked.
-          link.onclick = function (e) {
-            const clickedLayer = this.textContent;
+          link.onclick = (e) => {
+            const clickedLayer = link.textContent;
             e.preventDefault();
             e.stopPropagation();
 
-            const visibility = map.getLayoutProperty(
+            const visibility = this.map.getLayoutProperty(
                 clickedLayer,
                 'visibility'
             );
 
 // Toggle layer visibility by changing the layout object's visibility property.
             if (visibility === 'visible') {
-              map.setLayoutProperty(clickedLayer, 'visibility', 'none');
-              this.className = '';
+              this.map.setLayoutProperty(clickedLayer, 'visibility', 'none');
+              link.className = '';
             } else {
-              this.className = 'active';
-              map.setLayoutProperty(
+              link.className = 'active';
+              this.map.setLayoutProperty(
                   clickedLayer,
                   'visibility',
                   'visible'
@@ -146,11 +170,11 @@ export default {
           layers.appendChild(link);
         }
       });
-      map.on('mouseenter', ['stations', 'stops'], () => {
-        map.getCanvas().style.cursor = 'pointer';
+      this.map.on('mouseenter', ['stations', 'stops'], () => {
+        this.map.getCanvas().style.cursor = 'pointer';
       })
-      map.on('mouseleave', ['stations', 'stops'], () => {
-        map.getCanvas().style.cursor = '';
+      this.map.on('mouseleave', ['stations', 'stops'], () => {
+        this.map.getCanvas().style.cursor = '';
       });
     },
   }
